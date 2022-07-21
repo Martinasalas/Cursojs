acc925e4888b07de516de0707a76306b48db0e7e
Martina Salas <martinasalas08@gmail.com>
2022-07-20 22:03:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let ingreseAnioNacimiento = prompt ("Ingresa tu anio de nacimiento");
+const anioActual = 2022;
+let anioNacimiento = (anioActual - ingreseAnioNacimiento);
+
+
+do {(alert ("Debes ingresar tu anio de nacimiento para seguir"))
+   ingreseAnioNacimiento = prompt ("Ingresa tu anio de nacimiento")}
+
+while  (ingreseAnioNacimiento == "")
+
+if  (anioNacimiento >= 18 ) {
+   alert ("Felicitaciones! podes navegar en nuestro sitio");
+}
+   else{ 
+      alert ("Lo sentimos! necesitas ser mayor de edad para navegar en nuestro sitio");
+   }
