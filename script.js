7d057b6af7c14d4e3bf4369345a79513237a7fe6
Martina Salas <martinasalas08@gmail.com>
2022-07-20 22:13:35 -0400

    ultimo cambio

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const anioActual = 2022;
 let anioNacimiento = (anioActual - ingreseAnioNacimiento);
 
 
-do {(alert ("Debes ingresar tu anio de nacimiento para seguir"))
+do {(alert ("Debes ingresar tu anio de nacimiento para seguir"));
    ingreseAnioNacimiento = prompt ("Ingresa tu anio de nacimiento")}
 
-while  (ingreseAnioNacimiento == "")
+while  (ingreseAnioNacimiento == "");
+do {alert ("debes ingresar un numero");
+   ingreseAnioNacimiento = prompt ("Ingresa tu anio de nacimiento")}
+while (isNaN(ingreseAnioNacimiento))
+   
 
 if  (anioNacimiento >= 18 ) {
    alert ("Felicitaciones! podes navegar en nuestro sitio");
