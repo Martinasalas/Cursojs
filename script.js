09301065474e7c4b790aaf0afa1999fc84e11b45
Martina Salas <martinasalas08@gmail.com>
2022-08-25 14:00:33 -0400

    cambios

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ let altura = prompt ("Ingresa tu altura en METROS");
 const altura2 = altura*altura 
 
 const imcfinal = peso / altura2 
-alert('tu IMC es igual a ' + imcfinal)
+ if (imcfinal<18.5) {
+ alert ('Tu IMC es de' + imcfinal + 'Tu peso esta por debajo de lo normal')
+ } else if(imcfinal > 18.5 && imcfinal < 25) {
+ alert ('Tu IMC es de' + imcfinal + 'Tu peso es normal')
+ } else if(imcfinal > 24.9  && imcfinal < 30 ) {
+ alert ('Tu IMC es de' + imcfinal + 'Tu peso esta por arriba de lo normal')
+ } else  if (imcfinal > 29.9 ) {
+ alert ('Tu IMC es de' + imcfinal + 'Tenes obesidad')
+ }
+
 
 
 }
